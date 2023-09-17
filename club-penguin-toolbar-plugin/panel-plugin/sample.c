d47b0dbf9b7efb4733e55ca9c61a49402d6c068e
schertudi <>
2023-09-18 05:23:03 +1200

    copyright update

--- club-penguin-toolbar-plugin/panel-plugin/sample.c
+++ club-penguin-toolbar-plugin/panel-plugin/sample.c
@@ -1,6 +1,7 @@
 /*  $Id$
  *
  *  Copyright (C) 2019 John Doo <john@foo.org>
+ *  Copyright (C) 2023 schertudi
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
